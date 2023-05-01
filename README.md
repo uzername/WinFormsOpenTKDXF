1d899e6b94bc773494c0b3c4ec4c825973f686be
uzername <ivanpost777@gmail.com>
2023-05-01 15:13:16 +0300

    added OpenGL setting. Pretty basic.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WinFormsOpenTKDXF
+
+Here be a control for WinForms to show dxf file in 3D, using OpenTK
---
+++ UserControlWinFormsDXF.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace WinFormsOpenTKDXF
+{
+    partial class UserControlWinFormsDXF
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UserControlWinFormsDXF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UserControlWinFormsDXF";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
